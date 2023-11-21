e5ec003d2adbb13f1a63e09ba4620b163bfdf7ce
Omar Hashimi <omar.hashime786@gmail.com>
2023-11-21 13:56:32 +0430

    fixed routing bugs

--- src/components/Home/logo/Logo.jsx
+++ src/components/Home/logo/Logo.jsx
@@ -1,33 +1,14 @@
 import "./logo.scss"
 import logo from "../../../assets/images/m1.png"
-import { useEffect, useRef } from "react"
-import gsap from "gsap-trial"
-import DrawSVGPlugin from "gsap-trial/DrawSVGPlugin"
+import {  useRef } from "react"
+
 
 function Logo() {
     const bgRef = useRef()
     const logoRef = useRef()
 
 
-    useEffect(()=>{
-        gsap.registerPlugin(DrawSVGPlugin)
-
-        gsap.timeline()
-        .to(bgRef.current, {
-            duration: 1,
-            opacity: 1
-        })
-
-        gsap.fromTo(
-            logoRef.current,{
-                opacity: 0
-            },{
-                opacity: 1,
-                duration: 4,
-                delay: 4
-            }
-        )
-    },[])
+    
 
   return (
     <div className="logo-container" ref={bgRef}>
